e63c274c1600381342b1283f3602943f5968ddd6
maymyintmyatthusm30423-source <maymyintmyatthu.sm30423@gmail.com>
2026-01-09 17:59:48 +0630

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,20 +1,24 @@
-# Python  simple Calculator
+#Python Simple Calculator
+while True:
+    operator = input("Enter an operator (+ - * /) or q to quit: ")
 
-operator = input("Enter an operator(+-*/): ")
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+    if operator == "q":
+        print("Goodbye!")
+        break
 
-if operator == "+":
-    result = num1 + num2
-    print(round(result))
-elif operator == "-":
-    result = num1 - num2
-    print(round(result))
-elif operator == "*":
-    result = num1 * num2
-    print(round(result))
-elif operator == "/":
-    result = num1 / num2
-    print(round(result, 2))
-else:
-    print(f"{operator} is not a valid operator")
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    if operator == "+":
+        print(round(num1 + num2))
+    elif operator == "-":
+        print(round(num1 - num2))
+    elif operator == "*":
+        print(round(num1 * num2))
+    elif operator == "/":
+        if num2 == 0:
+            print("Cannot divide by zero")
+        else:
+            print(round(num1 / num2, 2))
+    else:
+        print("Invalid operator")
